f41033e0ffe58ac0b3c9903b9058219a3d1011e2
Fmakgoka <fmakgoka@wethinkcode.student.co.za>
2020-07-28 01:09:20 +0200

    updates

--- routes/homepage.js
+++ routes/homepage.js
@@ -6,7 +6,8 @@ var db = require('../model/db');
 
 /*Get*/
 router.get('/', function (req, res, next) {
-    con.query('SELECT * FROM image', function (err, data) {
+    con.query('SELECT username,gender, biography, city, profileimage FROM users AS u \
+    INNER JOIN profile AS p ON u.id = p.profile_id INNER JOIN image AS i ON p.profile_id = i.img_id', function (err, data) {
         if (err) throw err;
         
         res.render('homepage', {userdata: data})
