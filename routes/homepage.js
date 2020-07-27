ee75cc62c588e4fd62718ee7d5d30fd89dc75915
mmasima <morapedimasima@gmail.com>
2020-07-28 00:27:36 +0200

    latest changes

--- routes/homepage.js
+++ routes/homepage.js
@@ -12,26 +12,5 @@ router.get('/', function (req, res, next) {
         res.render('homepage', {userdata: data})
     })
 });
-router.post('/', (req, res) => {
-    const upload = require('../logic/uploadImage');
-    var image = req.body.image;
-    image = image || [];
 
-    uploadImage.array('image', image.length)(req, res, (err) => {
-        if (err) {
-            if (err.code === 'LIMIT_UNEXPECTED_FILE') {
-                req.flash('error', 'Too many files, max files allowed ' + (5 - userImages.length));
-                return next();
-            } else {
-                console.log(err);
-                return res.status(500).send('Server error');
-            }
-        }
-
-        else {
-            console.log(req.file);
-            res.send('test');
-        }
-    });
-});
 module.exports = router;
