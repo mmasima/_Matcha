8efa5fedd6dd0b341f548ea2f3e18d3af8f8dcf5
Fmakgoka <50695653+Fmakgoka@users.noreply.github.com>
2020-07-29 14:12:24 +0200

    Revert "imageupload"

--- routes/profile.js
+++ routes/profile.js
@@ -2,76 +2,57 @@ var express = require('express');
 var router = express.Router();
 var con = require('../model/connection');
 const session = require('express-session');
-var db = require('../model/db');
-var upload = require('../logic/uploadImage');
-var multer = require("multer");
-
 
 router.get('/', function (req, res, next) {
     res.render('profile');
     var username = req.session.username;
 });
 
-router.post('/', async function (req, res) {
+router.post('/', function (req, res) {
+    var id = req.session.GetId;
+    if (req.method == "POST") {
 
-    // var id = req.session.GetId;
+        var gender = req.body.gender;
+        var age = req.body.age;
+        var preference = req.body.SelectPreference;
+       var interests = req.body.interests;
+        var bio = req.body.bio;
+        var image = req.body.image;
+        var complete = 'yes';
 
-    // var gender = req.body.gender;
-    // var age = req.body.age;
-    // var preference = req.body.SelectPreference;
-    // var interests = req.body.interests;
-    // var bio = req.body.bio;
-    // var image = req.body.image;
-    // var complete = 'yes';
+        if (!age || !gender || !bio || !interests) {
 
-    // // if (!age || !gender || !bio || !interests) {
-
-    //     res.status("400");
-    //     res.redirect('/')
-    //     console.log("oops! something went wrong");
-    //     console.log("some fields may be missing");
-    // }
-    //  else {
-
-    try {
-        upload(req, res, async (err) => {
-            var id = req.session.GetId;
-
-            var gender = req.body.gender;
-            var age = req.body.age;
-            var preference = req.body.SelectPreference;
-            var interests = req.body.interests;
-            var bio = req.body.bio;
-            var image = req.body.image;
-            var complete = 'yes';
-
-            if (err instanceof multer.MulterError) {
-                res.render('/profile', {
-                    error: {
-                        message: "An error occured uploading your images`",
-                    }
-                })
-            }
-            else if (err) {
-                res.send(err);
-            } else {
-                var profileimage = req.file;
-                console.log(profileimage);
-                var sql = await db.insertProfile(id, age, gender, preference, bio);
-                var insertInterests = await db.insertInterests(id, interests)
-                var profilecomplete = await db.updateProfileComplete(complete, id);
-                await db.insertPicture(id, profileimage);
-                res.redirect('homepage')
-            }
-        })
-    } catch (error) {
-        console.log(error)
-
-        //    }
-
+                res.status("400");
+                res.redirect('/')
+                console.log("oops! something went wrong");
+                console.log("some fields may be missing");
+        }
+        else {
+            var sql = "INSERT INTO profile \ (profile_id, age, gender, preference, biography) \
+            VALUES ('"+ id + "','" + age + "','" + gender + "','" + preference + "', '" + bio + "')";
+            con.query(sql, (err, result) => {
+                console.log("profile query");
+                if (err) throw err;
+                console.log("inserted profile details");
+                res.end();
+            });
+            var sql = "INSERT INTO interests \ (uid, interests) \
+            VALUES( '"+ id + "', '"+ interests + "')";
+            con.query(sql, (err, result) => {
+                console.log("interests query submitted");
+                if (err) throw err;
+                console.log("inserted interests details");
+            })
+            var sql =`UPDATE users SET profile_complete ='${complete}' where id = '${id}'`;
+            con.query(sql, (err, result) => {
+                if (err) throw err;
+                console.log(result);
+                console.log('set profile_complete to yes');
+            })
+            res.redirect('homepage')
+        }
+        res.redirect('/')
     }
-    //res.redirect('/')
-
 });
 
 module.exports = router;
