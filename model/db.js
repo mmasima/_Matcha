568f0f6287841366940ac6ea0c8139557bbc3321
Fmakgoka <fmakgoka@wethinkcode.student.co.za>
2020-07-30 02:08:29 +0200

    the search

--- model/db.js
+++ model/db.js
@@ -2,72 +2,72 @@ var con = require('./connection');
 
 let matcha = {};
 
-matcha.usernameV = function(username) {
-  const validUserPattern = /(?=^.{2,50}$)(?=.*[a-z]).*$/;
-  return username.match(validUserPattern);
+matcha.usernameV = function (username) {
+	const validUserPattern = /(?=^.{2,50}$)(?=.*[a-z]).*$/;
+	return username.match(validUserPattern);
 };
 
 matcha.emailV = (email) => {
-  const validEmailPattern = /[\w-]+@([\w-]+\.)+[\w-]+/;
-  return email.match(validEmailPattern);
+	const validEmailPattern = /[\w-]+@([\w-]+\.)+[\w-]+/;
+	return email.match(validEmailPattern);
 };
 
 matcha.passwordV = (password) => {
-  const validPassPattern = /(?=^.{6,100}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/;
-  return password.match(validPassPattern);
+	const validPassPattern = /(?=^.{6,100}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/;
+	return password.match(validPassPattern);
 };
 
 matcha.firstNameV = (name) => {
-  const validNamePattern = /(?=^.{2,50}$)^[A-Za-z]+$/;
-  return name.match(validNamePattern);
+	const validNamePattern = /(?=^.{2,50}$)^[A-Za-z]+$/;
+	return name.match(validNamePattern);
 };
 
 matcha.lastNameV = (name) => {
-  const validNamePattern = /(?=^.{2,50}$)^[A-Za-z]+$/;
-  return name.match(validNamePattern);
+	const validNamePattern = /(?=^.{2,50}$)^[A-Za-z]+$/;
+	return name.match(validNamePattern);
 };
 
-matcha.checkEmailAndUserNameExist= function(username, email){
-    return new Promise((resolve, reject) => {
+matcha.checkEmailAndUserNameExist = function (username, email) {
+	return new Promise((resolve, reject) => {
 		con.query('SELECT * FROM users WHERE username=? OR email=?',
 			[username, email],
 			(error, result) => {
 				if (error) {
 					return reject(error);
-                }
-                console.log(result)
-                return resolve(result);
-                
+				}
+				console.log(result)
+				return resolve(result);
+
 			})
 	})
 
 }
 
-matcha.findUserByToken= function(token){
-    return new Promise((resolve, reject) => {
+matcha.findUserByToken = function (token) {
+	return new Promise((resolve, reject) => {
 		con.query('SELECT * FROM users WHERE token=?',
 			[token],
 			(error, result) => {
 				if (error) {
 					return reject(error);
-                }
-                console.log(result)
-                return resolve(result);
-                
+				}
+				console.log(result)
+				return resolve(result);
+
 			})
 	})
 
 }
 
-matcha.updateUserPassword = function(password,username){
-    return new Promise((resolve, reject) => {
+matcha.updateUserPassword = function (password, username) {
+	return new Promise((resolve, reject) => {
 		con.query(`UPDATE users SET password=?,token='' WHERE username =?`,
-			[password,username],
+			[password, username],
 			(error, result) => {
 				if (error) {
 					return reject(error);
-                }
-                console.log(result);
+				}
+				console.log(result);
 				return resolve(result);
 			})
 	})
@@ -75,15 +75,15 @@ matcha.updateUserPassword = function(password,username){
 }
 
 
-matcha.activateAccount = function(token){
-    return new Promise((resolve, reject) => {
+matcha.activateAccount = function (token) {
+	return new Promise((resolve, reject) => {
 		con.query(`UPDATE users SET verify=?,token='' WHERE token =?`,
-			['yes',token],
+			['yes', token],
 			(error, result) => {
 				if (error) {
 					return reject(error);
-                }
-                console.log(result);
+				}
+				console.log(result);
 				return resolve(result);
 			})
 	})
@@ -106,17 +106,32 @@ matcha.getUsers = function (gender, preference, city, commontag, famerating) {
 			INNER JOIN profile AS p ON u.id = p.profile_id INNER JOIN image AS i ON p.profile_id = i.img_id \
 			INNER JOIN interests AS n ON i.img_id = n.uid\
 			WHERE gender=? AND preference=? AND city=? AND interests=? AND famerating=?',
-			[tagetgender, gender, city, commontag,famerating],
+			[tagetgender, gender, city, commontag, famerating],
 			(error, result) => {
 				if (error) {
 					return reject(error);
 
 
 				}
-				console.log(result)
+				//console.log(result)
 				return resolve(result);
 
 			})
+	})
+
+}
+
+matcha.search = function () {
+	return new Promise((resolve, reject) => {
+		con.query(`SELECT * FROM users AS u INNER JOIN profile AS p ON u.id = p.profile_id 
+		INNER JOIN interests AS i ON p.profile_id = i.uid INNER JOIN image AS m ON i.uid = m.img_id`,
+		(error, result) => {
+			if (error) {
+				return reject(error);
+			}
+			//console.log(result);
+			return resolve(result);
+		})
 	})
 
 }
--- routes/homepage.js
+++ routes/homepage.js
@@ -7,13 +7,26 @@ var db = require('../model/db');
 /*Get*/
 router.get('/', async function (req, res, next) {
     try {
+        var id = req.session.GetId;
         let data = await db.getUsers('Male','Female', 'Randburg', '#gaming',5);
-        console.log(data)
         res.render('homepage', {userdata: data})
-        
+
     } catch (error) {
         console.log('error updating profile ',error.message)
     }
 });
+
+router.post('/',async function(req, res){
+    try {
+        //let result = _.filter(req.body, {} )
+        console.log(req.body)
+         let search = await db.search();
+        // _.filter(search, )
+        
+         res.redirect('homepage');
+    } catch (error) {
+        console.log('error search ',error.message)
+    }
+})
 
 module.exports = router;
