0ae96e7f7cb397ac55a190c27b4e1cc3fc2ee87d
mmasima <morapedimasima@gmail.com>
2020-08-02 18:10:07 +0200

    quick fix!

--- API/getuser.js
+++ API/getuser.js
@@ -1,50 +1,47 @@
-var express = require('express')
+var express = require("express");
 var router = express.Router();
-var con = require('../model/connection');
-var db = require('../model/db')
+var con = require("../model/connection");
+var db = require("../model/db");
 var global;
 
 /* GET home page. */
-router.get('/:username', async function (req, res) {
-    try {
-        let user = await db.getUserByUsername(req.params.username)
-        global = user;
-        console.log(global);
-        res.render('viewprofile', {
-             data: user,
-             })
-    } catch (error) {
-        console.log(error)
-    }
-    res.render('viewprofile', { message: req.flash('message') });
+router.get("/:username", async function (req, res) {
+  try {
+    let user = await db.getUserByUsername(req.params.username);
+    global = user;
+    console.log(global);
+    res.render("viewprofile", {
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  res.render("viewprofile", { message: req.flash("message") });
 });
 
+router.post("/", function (req, res) {
+  if (req.method == "POST") {
+    var like = 1;
+    var id = global.id;
 
-router.post('/', function (req, res) {
-    if (req.method == 'POST') {
-        var like = 1;
-        var id = global.id;
-
-        var check = `SELECT * FROM profile WHERE profile_id ='${id}'`
-        con.query(check, function (err, results, fields) {
-            if (err) throw err;
-            if (results[0].famerating <= 10) {
-                like = like + results[0].famerating;
-            }
-            else if (results[0].famerating == 10){
-                req.flash('message', 'you liked this person!');
-                res.redirect('/homepage');
-            }
+    var check = `SELECT * FROM profile WHERE profile_id ='${id}'`;
+    con.query(check, function (err, results, fields) {
+      if (err) throw err;
+      if (results[0].famerating <= 9) {
+          
+        like = like + results[0].famerating;
         var iLike = `UPDATE profile SET famerating='${like}' where profile_id = '${id}'`;
         con.query(iLike, (err, result) => {
-            if(err) throw err;
-            req.flash('message', 'you liked this person!');
-            res.redirect('/homepage');
-            })
+          if (err) throw err;
+          req.flash("message", "you liked this person!");
+          res.redirect("/homepage");
         });
-    };
+      } else {
+        req.flash("message", "you liked this person!");
+        res.redirect("/homepage");
+      }
+    });
+  }
 });
-
-
 
 module.exports = router;
